f9412b006bb477ff5af95a8eefc96c6ed842a597
Colin LEVERGER <colinleverger@gmail.com>
2016-04-22 18:44:31 +0200

    Textures OK

--- obj/camera.js
+++ obj/camera.js
@@ -1,31 +1,31 @@
 camera.prototype= new worldObject;
 function camera(parent)
 {
-	this.base = worldObject;
-	this.base(parent);
-	this.x = 0;
-	this.z = 0;
-	this.height = 0;
+    this.base = worldObject;
+    this.base(parent);
+    this.x = 0;
+    this.z = 0;
+    this.height = 0;
 }
 
 camera.prototype.draw = function()
 {
-	mat4.multiply(mvMatrix, this.localTransformation);
+    mat4.multiply(mvMatrix, this.localTransformation);
 
-	setMatrixUniforms();
+    setMatrixUniforms();
 
-	//draws children
-	for(var i =0; i< this.children.length; i++)
-	{
-		this.children[i].draw();
-	}
+    //draws children
+    for(var i =0; i< this.children.length; i++)
+    {
+        this.children[i].draw();
+    }
 }
 
 camera.prototype.animate = function(elapsedTime)
 {
-	//animate children
-	for(var i =0; i< this.children.length; i++)
-	{
-		this.children[i].animate();
-	}
+    //animate children
+    for(var i =0; i< this.children.length; i++)
+    {
+        this.children[i].animate();
+    }
 }
--- obj/triangle.js
+++ obj/triangle.js
@@ -2,34 +2,34 @@ triangle.prototype = new worldObject;
 
 function triangle(parent)
 {
-	this.base = worldObject;
-	this.base (parent);
-	this.vertexPositionBuffer = this.initVertexPositionBuffer();
-	this.vertexTextureCoordBuffer = this.initTextureCoordPositionBuffer();
+    this.base = worldObject;
+    this.base (parent);
+    this.vertexPositionBuffer = this.initVertexPositionBuffer();
+    this.vertexTextureCoordBuffer = this.initTextureCoordPositionBuffer();
 }
 triangle.prototype.initVertexPositionBuffer = function()
 {
-	var vertices = [
-		 0.0,  1.0,  0.0,
-		-1.0, -1.0,  0.0,
-		 1.0, -1.0,  0.0];
-	var vertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	vertexPositionBuffer.itemSize = 3;
-	vertexPositionBuffer.numItems = 3;
-	return vertexPositionBuffer;
+    var vertices = [
+         0.0,  1.0,  0.0,
+        -1.0, -1.0,  0.0,
+         1.0, -1.0,  0.0];
+    var vertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    vertexPositionBuffer.itemSize = 3;
+    vertexPositionBuffer.numItems = 3;
+    return vertexPositionBuffer;
 }
 triangle.prototype.initTextureCoordPositionBuffer = function()
 {
-	var textureCoords = [
-		1.0, 0.0,
-		0.0, 1.0,
-		0.0, 0.0,];
-	vertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexTextureCoordBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-	vertexTextureCoordBuffer.itemSize = 2;
-	vertexTextureCoordBuffer.numItems = 3;
-	return vertexTextureCoordBuffer;
+    var textureCoords = [
+        1.0, 0.0,
+        0.0, 1.0,
+        0.0, 0.0,];
+    vertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTextureCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    vertexTextureCoordBuffer.itemSize = 2;
+    vertexTextureCoordBuffer.numItems = 3;
+    return vertexTextureCoordBuffer;
 }
